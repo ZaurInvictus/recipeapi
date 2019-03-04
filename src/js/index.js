a0e1e4dcc91f96892485b9b745768a57194ba4ad
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-03-04 17:03:40 -0600

    Ajax Loader added

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //CONTROLLER FILE 
 import Search from './models/Search'; 
 import * as searchView from './views/searchView';
-import { elements } from './views/base'; 
+import { elements, renderLoader, clearLoader } from './views/base'; 
 
 /** GLOBAL STATE OF THE APP
  * -Search object
@@ -21,11 +21,15 @@ const controlSearch = async () => {
     state.search = new Search(query);
 
     // 3) Prepare UI for results
-    
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render Results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 }
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,8 +1,39 @@
-import {
-  elements
-} from './base';
+import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => { 
+  elements.searchInput.value = '';
+};
+
+export const clearResults = () => {
+  elements.searchResultList.innerHTML = '';
+};
+
+
+/*
+//'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5;  acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9;  acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15;  acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 18;  acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+   if(title.length > limit) {
+      title.split('').reduce((acc, cur) => {
+        if(acc + cur.length <= limit) {
+          newTitle.push(cur);
+        }
+        return acc + cur.length;
+      }, 0);
+
+      //return the result and join (using join method) words of the array into a string separated by spaces
+      return `${newTitle.join('')} ...`;
+   }
+   return title;
+}
 
 const renderRecipe = recipe => {
   const markup = `
@@ -12,15 +43,15 @@ const renderRecipe = recipe => {
           <img src="${recipe.image_url}" alt="${recipe.title}">
       </figure>
       <div class="results__data">
-          <h4 class="results__name">${recipe.title}</h4>
+          <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
           <p class="results__author">${recipe.publisher}</p>
       </div>
   </a>
 </li>
   `;
   elements.searchResultList.insertAdjacentHTML('beforeend', markup);
-}
+};
 
 export const renderResults = recipes => {
   recipes.forEach(renderRecipe);
-}
+};
