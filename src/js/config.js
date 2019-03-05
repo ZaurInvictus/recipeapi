05ca5c36efc6dfd6f1d4330eb21f48280a169bdc
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-03-05 17:45:57 -0600

    page pagination added

---
+++ src/js/config.js
@@ -0,0 +1,1 @@
+const key = 'aaf7d29b8b8de4c538d843007a853d23'; 
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { key } from '../config';
 
 export default class Search {
   constructor(query) {
@@ -6,7 +7,6 @@ export default class Search {
   }
 
   async getResults() {
-    const key = 'aaf7d29b8b8de4c538d843007a853d23'; 
     try {
       const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
       this.result = res.data.recipes;
